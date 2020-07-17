66b75a150d679df25b93fe4976a10ab4036cdc80
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-07-17 08:00:32 -0400

    Update jay.lua

--- TIC-80/jay.lua
+++ TIC-80/jay.lua
@@ -20,17 +20,6 @@ function TIC()
 	t=t+1
 end
 
--- <TILES>
--- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
--- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
--- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
--- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
--- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
--- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
--- </TILES>
-
 -- <WAVES>
 -- 000:00000000ffffffff00000000ffffffff
 -- 001:0123456789abcdeffedcba9876543210
@@ -42,6 +31,6 @@ end
 -- </SFX>
 
 -- <PALETTE>
--- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- 000:140c1cff7b00fcfcfc9492b54a4a4a5e480de1ff00d22d2d0008ff00ff9dffdd00949494d327ae0292cf12520af0e8b2
 -- </PALETTE>
 
