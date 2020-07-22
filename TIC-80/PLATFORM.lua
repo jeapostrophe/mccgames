511d91d768ac6e2c190308a1f48d6294ce64a5fd
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-07-22 18:59:36 -0400

    fixed some bugs

---
+++ TIC-80/PLATFORM.lua
@@ -0,0 +1,36 @@
+-- title:  game title
+-- author: game developer
+-- desc:   short description
+-- script: lua
+
+t=0
+x=96
+y=24
+
+function TIC()
+
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+	cls(13)
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <PALETTE>
+-- 000:0000043c3c3c898989ffffffffa5e6ff0000ff8104ffda0404da040000cef2ba918544005d0c00007100000055790079
+-- </PALETTE>
+
