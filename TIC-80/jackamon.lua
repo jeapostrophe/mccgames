1f50a179e255707036bfac227f50944132967034
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-08-24 15:22:27 -0400

    Update jackamon.lua

--- TIC-80/jackamon.lua
+++ TIC-80/jackamon.lua
@@ -2,8 +2,9 @@
 -- author: Balistic Ghoul Studios
 -- desc:  A game like pokemon (See PICO-8 for my other games)
 -- script: lua
---DEBUG=false
-DEBUG=true
+
+DEBUG=false
+--DEBUG=true
 
 B_OK=5
 B_BACK=4
