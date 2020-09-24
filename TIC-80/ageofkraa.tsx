e81bbbd5089e477a4c601ec5969b27e122a6bff1
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-09-24 07:43:36 -0400

    did stuff

---
+++ TIC-80/ageofkraa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ageofkraa" tilewidth="8" tileheight="8" tilecount="512" columns="16">
+ <image source="ageofkraa.png" width="128" height="256"/>
+</tileset>
---
+++ fantasyhextiles_randr_4_v1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="fantasyhextiles_randr_4_v1" tilewidth="32" tileheight="28" tilecount="64" columns="8">
+ <tileoffset x="0" y="2"/>
+ <image source="../fantasyhextiles_randr_4_v1.png" width="256" height="240"/>
+</tileset>
