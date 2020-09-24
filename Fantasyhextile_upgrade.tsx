0068aac2cf3b7481213285f941969e3f7aec5b50
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-09-24 11:28:38 -0400

    did things

---
+++ Fantasyhextile_upgrade.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Fantasyhextile_upgrade" tilewidth="32" tileheight="48" tilecount="192" columns="16">
+ <image source="../Fantasyhextile_upgrade.png" width="512" height="576"/>
+</tileset>
---
+++ Rivers bonus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Rivers bonus" tilewidth="32" tileheight="48" tilecount="220" columns="22">
+ <image source="../Rivers bonus.png" width="704" height="480"/>
+</tileset>
