69d8b99b81445598d8017be3e9ddb0f94358e332
Jack McCarthy <jack.day.mccarthy@gmail.com>
2020-10-15 06:31:51 -0400

    LOTS of stuff done

---
+++ TIC-80/BattleOfTheBills.lua
@@ -0,0 +1,41 @@
+-- title:  game title
+-- author: game developer
+-- desc:   short description
+-- script: lua
+
+t=0
+x=96
+y=24
+
+function TIC()
+
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+	cls(13)
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
+
+-- <TILES>
+-- 001:00070000000700000077700000eee0000ef7fe000ef7fe00eef7fee0eeeeeee0
+-- 002:0009000000999000099799000977790097f7f79097f7f79077f7f77077777770
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <PALETTE>
+-- 000:0000001d2b537e255383769cab5236008751ff004d5f574fff77a8ffa300c2c3c700e436ffccaa29adffffec27fff1e8
+-- </PALETTE>
+
